b28bf91f164196b415ca552cfb5d16ae2e7f3c9a
AISwitzerland <wehrlinatasha@gmail.com>
2025-02-18 22:56:25 +0100

    fix: Aktualisiere Babel und Next.js Konfiguration für SWC Kompatibilität

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+  ],
+  // Nur für Test-Umgebung aktivieren
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+        ['@babel/preset-react', { runtime: 'automatic' }],
+      ],
+    },
+  },
+}; 
