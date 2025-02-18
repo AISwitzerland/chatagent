9e3a8281833acbebf2bc903c48687e205b2f7faa
AISwitzerland <wehrlinatasha@gmail.com>
2025-02-19 00:18:06 +0100

    fix: Stelle Konfigurationsdateien zurück auf CommonJS

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
